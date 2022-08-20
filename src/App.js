acbad6f523ac42901cb0278df7deff82d0d5ab7b
Proloycb <proloychacroborty@gmail.com>
2022-08-20 09:24:00 +0600

    header and footer implement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Header from './pages/Shared/Header';
+import Footer from './pages/Shared/Footer';
 
 function App() {
   return (
     <div className="max-w-7xl mx-auto">
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
