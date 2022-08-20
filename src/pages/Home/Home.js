38c68b9a34db7bf6289ccc1ee88ef9df85452e90
Proloycb <proloychacroborty@gmail.com>
2022-08-20 13:00:48 +0600

    one extra part are completed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
+import Contact from './Contact';
 import Services from './Services';
 
 const Home = () => {
     return (
         <div>
-            <Banner/>
-            <Services/>
+            <Banner />
+            <Services />
+            <Contact />
         </div>
     );
 };
